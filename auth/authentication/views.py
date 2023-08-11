3538f74220dcc1330a46a43c99c5b8e2113d67a0
Dmitry Gubich <gubich97@gmail.com>
2023-08-12 00:42:15 +0200

    broker part

--- auth/authentication/views.py
+++ auth/authentication/views.py
@@ -1,3 +1,6 @@
+import json
+
+from app.producer import produce
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
@@ -12,6 +15,13 @@ class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        user = super().post(request, *args, **kwargs)
+        event = {"public_id": 123213, **request.data}
+        payload = json.dumps(event)
+        produce(payload=payload)
+        return user
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
